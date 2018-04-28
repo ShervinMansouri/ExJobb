3f6f4346e8594c5f791b3e5c6ccbfe5c1887f6da
khovstadius <khovstadius@hotmail.com>
2018-04-28 20:47:02 +0200

    Added a lot
    7 canvas
    buttons that controols when a canvas can be played
    text quest 1 and 2

--- ExJobb/ExJobb/Assets/AppScripts/CanvasController.cs
+++ ExJobb/ExJobb/Assets/AppScripts/CanvasController.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 using Mapbox.Unity.Location;
 
 public class CanvasController : MonoBehaviour
@@ -11,20 +11,35 @@ public class CanvasController : MonoBehaviour
     public Canvas m_canvas;
     public float m_minLatitude, m_maxLatitude, m_minLongitude, m_maxLongitude;
 
+    public Button button;
 
+    public bool canBePlayed = false;
+    
 	// Use this for initialization
 	void Start ()
     {
         m_canvas.enabled = false;
-        
+        //Button btn = button.GetComponent<Button>();
+        button.onClick.AddListener(TaskOnClick);
+
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    public void setBool(bool set )
+    {
+        canBePlayed = set;
+    }
+    void TaskOnClick()
+    {
+        canBePlayed = true;
+        Debug.Log("You have clicked the button!");
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
         Location currLoc = m_locationProvider.CurrentLocation;
 
-        if (currLoc.LatitudeLongitude.x > m_minLatitude && currLoc.LatitudeLongitude.x < m_maxLatitude && currLoc.LatitudeLongitude.y > m_minLongitude && currLoc.LatitudeLongitude.y < m_maxLongitude)
+        if (currLoc.LatitudeLongitude.x > m_minLatitude && currLoc.LatitudeLongitude.x < m_maxLatitude && currLoc.LatitudeLongitude.y > m_minLongitude && currLoc.LatitudeLongitude.y < m_maxLongitude && canBePlayed)
         {
             m_canvas.enabled = true;
         }
--- ExJobb/ExJobb/Assets/AppScripts/GameController.cs
+++ ExJobb/ExJobb/Assets/AppScripts/GameController.cs
@@ -2,7 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
+    public bool Quest1Finished = false;
+    public bool Quest2Finished = false;
+    public bool Quest3Finished = false;
+    public bool Quest4Finished = false;
+    public bool Quest5Finished = false;
+    public bool Quest6Finished = false;
+    public bool Quest7Finished = false;
+
 
     // Use this for initialization
     void Start()
